49266e5cd2eb4bfcfd31efe40c19b715c3fa961f
arshiamufti <amufti16@gmail.com>
2018-02-04 14:36:35 -0500

    ex 1 and 2

--- exercises/gigasecond/src/lib.rs
+++ exercises/gigasecond/src/lib.rs
@@ -3,5 +3,6 @@ use chrono::*;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!()
+    let gigasecond = 1000000000;
+    start.checked_add_signed(Duration::seconds(gigasecond)).unwrap()
 }
