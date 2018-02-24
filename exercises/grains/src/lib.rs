e5ebdf2ea6610f9f97c54ad4ed41fd27f221fcc0
arshiamufti <amufti16@gmail.com>
2018-02-23 19:23:49 -0500

    yay high school math

--- exercises/grains/src/lib.rs
+++ exercises/grains/src/lib.rs
@@ -1,7 +1,14 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!();
+    if s < 1 || s > 64 {
+        panic!("Square must be between 1 and 64");
+    }
+    2u64.pow(s-1)
 }
 
+/*
+ * the sum of the first 64 terms of the geometric series is 1, 2, 4,... is
+ * 2^(64) - 1
+ */
 pub fn total() -> u64 {
-    unimplemented!();
+    std::u64::MAX
 }
--- exercises/sum-of-multiples/src/lib.rs
+++ exercises/sum-of-multiples/src/lib.rs
@@ -1,3 +1,16 @@
+use std::collections::HashSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!()
+    let mut r = HashSet::new();
+
+    for i in 1..limit {
+        for &f in factors {
+            if (i >= f) && (i % f == 0) {
+                println!("{}", i);
+                r.insert(i);
+            }
+        }
+    }
+
+    r.iter().sum()
 }
